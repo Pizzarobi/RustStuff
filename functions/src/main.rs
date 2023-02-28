1b273e07b922bd969baa341e94b7b09d4bd89861
Robert Kalmar <git@robert-k.net>
2023-02-28 13:10:36 +0100

    ownership

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,17 +1,34 @@
 fn main() {
-    let y = another_function(3,2);
-    println!("The value of y is {}",y);
+    // let y = another_function(3,2);
+    // println!("The value of y is {}",y);
 
-    if y < 5{
-        println!("1");
-    }else if y == 5 {
-        println!("2");
-    } else {
-        println!("3");
-    }
+    // if y < 5{
+    //     println!("1");
+    // }else if y == 5 {
+    //     println!("2");
+    // } else {
+    //     println!("3");
+    // }
+
+    reverse_countdown();
 }
 
 fn another_function(x: i32, y: i32) -> i32{
     println!("The value of x is {} and y is {}",x,y);
     x+y
+}
+
+fn iterate_through_array(){
+    let a = [5,3,2,5,6];
+
+    for element in a {
+        println!("val: {element}");
+    }
+}
+
+fn reverse_countdown(){
+    for number in (1..5).rev() {
+        println!("{number}!");
+    }
+    println!("Liftoff!!!");
 }
--- functions/target/debug/functions.d
+++ functions/target/debug/functions.d
@@ -1,1 +1,1 @@
-D:\Stuff_4.0\Repos\Rust\functions\target\debug\functions.exe: D:\Stuff_4.0\Repos\Rust\functions\src\main.rs
+/home/robi/repos/RustStuff/functions/target/debug/functions: /home/robi/repos/RustStuff/functions/src/main.rs
